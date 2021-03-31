e5b54a25112d0acd66ad4fb68ef59697eb15d5e2
VemComPY <rafabios@users.noreply.github.com>
2021-03-31 18:23:04 -0300

    Update main.py

--- main.py
+++ main.py
@@ -9,20 +9,28 @@ def lambda_handler(event, context):
     es_host       = os.getenv('ES_HOST')
     es_password   = os.getenv('ES_PASSWORD')
     es_login      = os.getenv('ES_LOGIN')
-    es_ssl        = os.getenv('ES_SSL')
-    es_ssl_verify = os.getenv('ES_SSL_VERIFY')
+    es_ssl        = bool(os.getenv('ES_SSL'))
+    es_ssl_verify = bool(os.getenv('ES_SSL_VERIFY'))
     es_except_indices = os.getenv('ES_EXCEPT_INDICES')
     es_unit        = os.getenv('ES_UNIT')
     es_unit_count  = int(os.getenv('ES_UNIT_COUNT'))
-
-    es = Elasticsearch(
+    
+    if es_login:
+     es = Elasticsearch(
+    hosts = [{'host': es_host , 'port': 443}],
+    use_ssl = es_ssl,
+    verify_certs = es_ssl_verify,
+    connection_class = RequestsHttpConnection
+    )
+    else:
+     es = Elasticsearch(
     hosts = [{'host': es_host , 'port': 443}],
     http_auth = (es_login, es_password),
     use_ssl = es_ssl,
     verify_certs = es_ssl_verify,
     connection_class = RequestsHttpConnection
     )
-
+    
 
     index_list = curator.IndexList(es)
     # Delete the indices for the pattern yyyy-mm-dd* with creation_date greater than x days.
